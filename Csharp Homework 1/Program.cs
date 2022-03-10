a1d0427b850e41ba126b2f6343e51ca4a2685320
Gorjan.Kostadinovski <Gorjan.Kostadinovski@PALMYRA16>
2022-03-10 20:25:10 +0100

    Add project files.

---
+++ Csharp Homework 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Csharp_Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numOne = Console.ReadLine();
+            var operators = Console.ReadLine();
+            var numTwo = Console.ReadLine();
+            string operatorCase = operators;
+            int numOneInt = int.Parse(numOne);
+            int numTwoInt = int.Parse(numTwo);
+            
+
+            switch (operatorCase) {
+                case "+":
+                    Console.WriteLine(numTwoInt + numTwoInt);
+                    break;
+                case "-":
+                    Console.WriteLine(numOneInt - numTwoInt);
+                    break;
+                case "*":
+                    Console.WriteLine(numOneInt * numTwoInt);
+                    break;
+                case "/":
+                    Console.WriteLine(numOneInt / numTwoInt);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator input");
+                    break;
+            }
+        }
+    }
+}
