c6fb62a6ddddd04e9618633bb4b9df35e727e87e
GorjanKostadinovski <gorjankostadinovski@gmail.com>
2022-03-12 20:39:34 +0100

    Vezba 2

---
+++ Csharp Homework 1/Vezba 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Vezba_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesi Cetiri Broja");
+            var numOne = Console.ReadLine();
+            var numTwo = Console.ReadLine();
+            var numThree = Console.ReadLine();
+            var numFour = Console.ReadLine();
+
+            double firstNum = int.Parse(numOne);
+            double secondNum = int.Parse(numTwo);
+            double thirdNum = int.Parse(numThree);
+            double fourthNum = int.Parse(numFour);
+
+            double result = (firstNum + secondNum + thirdNum + fourthNum) / 4;
+            Console.WriteLine(result);
+        }
+    }
+}
