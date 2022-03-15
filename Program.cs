23f4806ecb961c7ffb7f796ec1723804cbf566e4
GorjanKostadinovski <96490632+GorjanKostadinovski@users.noreply.github.com>
2022-03-15 19:45:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = new int[6];
+
+            Console.WriteLine("Vnesi go prviot Broj");
+            arrayOne[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vnesi go Vtoriot broj");
+            arrayOne[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vnesi go Tretijot broj");
+            arrayOne[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vnesi go Cetvrtiot broj");
+            arrayOne[3] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vnesi go Petiot Broj");
+            arrayOne[4] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vnesi go sestiot broj");
+            arrayOne[5] = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                if (arrayOne[i] % 2 == 0)
+                {
+                    sum += arrayOne[i];
+
+                }
+
+
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
