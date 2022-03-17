09fcb352c86cb5ac6e6479e4c3a31cc329ee903d
GorjanKostadinovski <96490632+GorjanKostadinovski@users.noreply.github.com>
2022-03-17 12:18:41 +0100

    Update Vezba 2.cs

--- Csharp Homework 1/Vezba 2/Vezba 2.cs
+++ Csharp Homework 1/Vezba 2/Vezba 2.cs
@@ -12,10 +12,10 @@ namespace Vezba_21
             var numThree = Console.ReadLine();
             var numFour = Console.ReadLine();
 
-            double firstNum = int.Parse(numOne);
-            double secondNum = int.Parse(numTwo);
-            double thirdNum = int.Parse(numThree);
-            double fourthNum = int.Parse(numFour);
+            double.TryParse(numOne,out double firstNum);
+            double.TryParse(numTwo,out double secondNum);
+            double.TryParse(numThree,out double thirdNum);
+            double.TryParse(numFour,out double fourthNum);
 
             double result = (firstNum + secondNum + thirdNum + fourthNum) / 4;
             Console.WriteLine(result);
