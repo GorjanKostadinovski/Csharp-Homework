181dddf510dcb04cbf4a8d465c4aac3aacab6b22
GorjanKostadinovski <gorjankostadinovski@gmail.com>
2022-03-17 13:31:34 +0100

    Vezba 3 Domasna 1

---
+++ Csharp Homework 1/Vezba 3/Vezba 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Vezba_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesi Go A");
+            string numOne = Console.ReadLine();
+            bool numOneParssed = int.TryParse(numOne, out int firstNum);
+            if (!numOneParssed)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            Console.WriteLine("Vnesi go B");
+            string numTwo = Console.ReadLine();
+          
+            bool numTwoParssed = int.TryParse(numTwo, out int secondNum);
+        
+            if (!numTwoParssed)
+            {
+                Console.WriteLine("Invalid Input");
+            }
+           
+            
+                int extraVar = firstNum;
+              
+                firstNum = secondNum;
+                secondNum = extraVar;
+                Console.WriteLine($"A= {firstNum}");
+                Console.WriteLine($"B= {secondNum}");
+            
+            
+        }
+    }
+}
