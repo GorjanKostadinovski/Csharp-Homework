dc8521cff199ee1724a82c9921d41cea2d25411f
GorjanKostadinovski <gorjankostadinovski@gmail.com>
2022-03-17 12:41:31 +0100

    Vezba 1 Domasna 2

---
+++ Csharp Homework 2/Vezba 1/Vezba 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Vezba_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOne = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Vnesi broj");
+                string num = Console.ReadLine();
+                bool checkParrsed = int.TryParse(num, out int convertedNum);
+                if (checkParrsed)
+                {
+                    arrayOne[i] = convertedNum;
+                }
+                else
+                {
+                    Console.WriteLine("Vnesovte Pogrsen broj ptobaj povtorno");
+                    i--;
+                }
+            }
+
+
+            int sumEven = 0;
+            foreach (int i in arrayOne)
+            {
+                if(i % 2 == 0)
+                {
+                    sumEven += i;
+                }
+            }
+            Console.WriteLine("\n Sumata na parnite broevi na nizata e " + sumEven);
+
+        }
+    }
+}
